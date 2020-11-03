dc48e932747dd4f35d32bcd5a2f3bbfac84ec9c1
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-03 19:54:00 +0000

    crud completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # add route to list all tasks
+  root to: 'tasks#index'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
   get '/tasks', to: 'tasks#index'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
 end
